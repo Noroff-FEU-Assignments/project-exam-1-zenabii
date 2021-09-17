a6292056271e6c5e29fd5a35ddd8518f6d5f0a51
zenabii <maria.turtles@outlook.com>
2021-09-17 11:25:10 +0200

    categories api and filter

--- dist/js/blog.js
+++ dist/js/blog.js
@@ -1,5 +1,7 @@
 const baseUrl = "https://projectexam.zenabi.no/wp-json/wp/v2/posts?_embed";
 const allArticles = document.querySelector(".all_articles");
+const latestImage = document.querySelector(".grid_image");
+
 
 async function getPosts(url){
     const response = await fetch(url);
@@ -8,7 +10,7 @@ async function getPosts(url){
         allArticles.innerHTML += `
         <div class="article">
         <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest articles" class="article_image">
-        <div class="hashtag_gategory">${post.categories}</div>
+        <div class="hashtag_gategory">#${post._embedded["wp:term"][0][0].name}</div>
         <h3 class="topic_name" id="latest_title">
         ${post.title.rendered}
         </h3>
@@ -20,3 +22,18 @@ async function getPosts(url){
 }
 
 getPosts(baseUrl);
+
+
+async function getImage(url){
+    const response = await fetch(url);
+    const post = await response.json();
+    post.forEach(function(post) {
+        latestImage.innerHTML += `
+        <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest post" class="latest_image">
+        
+        
+        `
+    });
+}
+
+getImage(baseUrl);
