f2520faf3bb70ab36d0861bef86991b1c5ed2991
zenabii <maria.turtles@outlook.com>
2021-09-19 22:17:20 +0200

    blog page api

--- dist/js/blog.js
+++ dist/js/blog.js
@@ -1,12 +1,19 @@
 const baseUrl = "https://projectexam.zenabi.no/wp-json/wp/v2/posts?_embed";
 const allArticles = document.querySelector(".all_articles");
 const latestImage = document.querySelector(".grid_image");
-
+const latestContent = document.querySelector(".latest_topic_grid_reverse");
+const readMore = document.querySelector(".read_more");
+var loadCount = 0;
 
 async function getPosts(url){
+    loadCount++;
     const response = await fetch(url);
     const posts = await response.json();
-    posts.forEach(function (post)  {
+    
+    posts.forEach(function (post, index)  {
+        if (index == 0) {
+            return;
+        }
         allArticles.innerHTML += `
         <div class="article">
         <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest articles" class="article_image">
@@ -14,26 +21,44 @@ async function getPosts(url){
         <h3 class="topic_name" id="latest_title">
         ${post.title.rendered}
         </h3>
-        
         </div>
-        
         `
     }); 
+    if (loadCount == 2) {
+        readMore.style.display = "none";
+    }
 }
 
 getPosts(baseUrl);
 
+readMore.onclick = () => getPosts("https://projectexam.zenabi.no/wp-json/wp/v2/posts?_embed&page=2")
+
+
+
+const postUrl = "https://projectexam.zenabi.no/wp-json/wp/v2/posts?_embed&filter[posts_per_page]=1";
 
 async function getImage(url){
     const response = await fetch(url);
-    const post = await response.json();
-    post.forEach(function(post) {
-        latestImage.innerHTML += `
-        <img src="${post._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest post" class="latest_image">
-        
-        
-        `
-    });
+    const latestPosts = await response.json();
+
+    latestContent.innerHTML = `
+        <div class="grid_image">
+            <img src="${latestPosts[0]._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest post" class="latest_image">
+        </div>
+        <div class="grid_content">
+                <div class="hashtags">
+                <a href="#" class="hashtag_gategory">#${latestPosts[0]._embedded["wp:term"][0][0].name}</a>
+            </div>
+            <h1 class="topic_name">
+            ${latestPosts[0].title.rendered}
+            </h1>
+            <p class="lead_paragraph">${latestPosts[0].excerpt.rendered.replace(/(<([^>]+)>)/gi, "")}</p>
+            <div class="hashtags">
+            <img src="img/arrow.svg" alt="read more button" class="arrow">
+            </div>
+        </div>
+    
+    `
 }
 
-getImage(baseUrl);
+getImage(postUrl);
