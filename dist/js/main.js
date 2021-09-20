ade25cba9974f68ce1d9231ce86187a105b6d641
zenabii <maria.turtles@outlook.com>
2021-09-20 11:36:02 +0200

    wip

--- dist/js/main.js
+++ dist/js/main.js
@@ -22,3 +22,34 @@ function toggleMenu() {
         showMenu = false;
     }
 }
+
+
+// const latestImage = document.querySelector(".grid_image");
+// const latestContent = document.querySelector(".latest_topic_grid");
+// const postUrl = "https://projectexam.zenabi.no/wp-json/wp/v2/posts?_embed&filter[posts_per_page]=1";
+
+// async function getImage(url){
+//     const response = await fetch(url);
+//     const latestPosts = await response.json();
+
+//     latestContent.innerHTML = `
+//         <div class="grid_image">
+//             <img src="${latestPosts[0]._embedded['wp:featuredmedia']['0'].source_url}" alt="Latest post" class="latest_image">
+//         </div>
+//         <div class="grid_content">
+//                 <div class="hashtags">
+//                 <a href="#" class="hashtag_gategory">#${latestPosts[0]._embedded["wp:term"][0][0].name}</a>
+//             </div>
+//             <h1 class="topic_name">
+//             ${latestPosts[0].title.rendered}
+//             </h1>
+//             <p class="lead_paragraph">${latestPosts[0].excerpt.rendered.replace(/(<([^>]+)>)/gi, "")}</p>
+//             <div class="hashtags">
+//             <img src="img/arrow.svg" alt="read more button" class="arrow">
+//             </div>
+//         </div>
+    
+//     `
+// }
+
+// getImage(postUrl);
